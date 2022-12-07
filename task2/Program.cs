7b192b48c609ec7948adeed9f77e270fcd9c917d
Helen <>
2022-12-07 16:06:13 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,3 +3,43 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+Console.WriteLine ("Введите число");
+
+bool isParsed = false;
+int number = 0;
+while (isParsed == false)
+{
+    isParsed = int.TryParse (Console.ReadLine (), out number);
+
+if (isParsed == false)
+
+{
+    Console.WriteLine ("Ошибка! Введите цифры!");
+} 
+}
+
+int result = 0;
+
+
+
+if (number >= 100)
+{
+    while (number  > 999)
+    {
+        number = number/10;
+    }
+
+    result = number % 10;
+
+Console.WriteLine (result);
+}
+
+
+else 
+{
+    Console.WriteLine ("Третьего числа не существует");
+}
+
+
